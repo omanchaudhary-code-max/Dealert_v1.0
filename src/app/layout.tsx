ea146eb503cf84d4f5dedc3fb02c22eb490b9938
omanchaudhary-code-max <omanchaudhary.work@gmail.com>
2026-06-28 21:07:08 +0545

    add: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Dealert - Nepal's Price Tracker & Deal Alert Platform",
   description: "Track prices, monitor drops, configure instant email and trigger alerts for items on Daraz e-commerce platforms in Nepal.",
+   icons: {
+    icon: '/dealert.ico',
+  },
 };
 
 export default function RootLayout({
